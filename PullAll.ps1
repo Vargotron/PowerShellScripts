b52088eb6bd9572078349ce989363207455ed0d7
Anthony Vargo <apvargo@gmail.com>
2016-02-25 15:40:25 -0600

    Added TargetBranch argument.

--- PullAll.ps1
+++ PullAll.ps1
@@ -1,14 +1,48 @@
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$False)]
+    [string]$TargetBranch = ""
+)
+$useTargetBranch = -Not ($TargetBranch -Eq "")
+# Save our pwd
+$current = pwd
+Write-Host "Discovering git repositories..." -ForegroundColor "Green"
+# RoboCopy is used to generate the repo list in order to get around the path size limitation
 RoboCopy . . /L /S /NJH /NJS |% {
-	If ($_ -match "(\w:\\.*)\\.git") {
+	If ($_ -Match "(\w:\\.*)\\.git") {
 		$matches[1]
 	}
 } |
+# Required for Get-Unique
 Sort-Object |
+# Only need the unique paths
 Get-Unique |
 ForEach {
-	cd $_;
-	$branch = git status |% { if ($_ -match "^On branch (.*)$") { $matches[1] } }
-	Write-Host "Pulling branch '$branch' from $_..." -ForegroundColor "Red";
-	git pull;
-	cd ..
+    # Enter the repo directory
+	cd $_
+    # Fetch branch info
+    git fetch
+    # Grab the current branch
+    $currentBranch = git status |% {
+        If ($_ -Match "^On branch (.*)$") {
+            $matches[1]
+        }
+    }
+    If ($useTargetBranch) {
+        $branch = $TargetBranch
+    } Else {
+        $branch = $currentBranch
+    }
+    # Switch branches if needed
+    If (-Not $branch -Eq $currentBranch) {
+        git checkout $branch
+    }
+	Write-Host "Pulling branch '$branch' from $_..." -ForegroundColor "Green"
+	git pull
+    # Checkout the current branch again
+    If (-Not $branch -Eq $currentBranch) {
+        git checkout $currentBranch
+    }
 }
+# Go back to the starting pwd
+cd $current
