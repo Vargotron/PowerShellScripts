44ebedff9caaf4b17833d91266168ef5d21ff9ca
Anthony Vargo <apvargo@gmail.com>
2016-02-26 12:49:06 -0600

    Add CleanTRUDI, use /m

--- BuildTRUDI.bat
+++ BuildTRUDI.bat
@@ -2,7 +2,7 @@
 ECHO(
 ECHO Building TRUDI...
 ECHO(
-PowerShell.exe -Command "Build.ps1 -MSBuildPath \"'C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe'\" -TargetSolutions 'C:\Code\TRUDI\sourcenew\UI\UI.sln','C:\Code\TRUDI\sourcenew\Authorization\Authorization.sln','C:\Code\TRUDI\sourcenew\TechObjects\TechObjects.sln' -ResetIIS"
+PowerShell.exe -Command "Build.ps1 -MSBuildPath \"'C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe'\" -TargetSolutions 'C:\Code\TRUDI\sourcenew\UI\UI.sln','C:\Code\TRUDI\sourcenew\Authorization\Authorization.sln','C:\Code\TRUDI\sourcenew\TechObjects\TechObjects.sln' -BuildOptions \"/m\" -ResetIIS"
 ECHO(
 ECHO Complete.
 ECHO(
