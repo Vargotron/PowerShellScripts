3dfb3e99b1458849f481f18cafe678f487e78226
Anthony Vargo <apvargo@gmail.com>
2016-02-26 11:00:24 -0600

    Added description and --no-merges to WhoWorkedOnThis

--- WhoWorkedOnThis.ps1
+++ WhoWorkedOnThis.ps1
@@ -1,3 +1,11 @@
+<#
+    WhoWorkedOnThis.ps1
+    -------------------
+
+    Scans the current git repository (recursively to a depth of 2 or the value given for the -Depth parameter) and
+    returns a list of all commit author email addresses, excluding merge commits. Use -DomainFilter to only return
+    author email addresses from a particular domain (include the @).
+#>
 [CmdletBinding()]
 Param (
     [Parameter(Mandatory=$False)]
@@ -7,7 +15,7 @@ Param (
 )
 RoboCopy . NULL /L /S /NDL /XX /NC /NS /NJH /NJS /FP /XA:H /XD .git /Lev:$Depth |
 ForEach {
-	git --no-pager log -- $_ |
+	git --no-pager log --no-merges -- $_ |
 	ForEach {
 		If ($_ -Match "^Author: (.*) <(.*)>$") {
 			$matches[2]
